f3c680bf62295735eb5d79fbf027fb354397be2b
NainaniJatinZ <nainani.jatin.0@gmail.com>
2021-09-15 00:15:26 +0530

    DS study plan

---
+++ leetcode/september/14-9/RemoveLinkedListElements.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        dum = ListNode(-1)
+        dum.next = head
+        current_node = dum
+        while current_node.next != None:
+            if current_node.next.val == val:
+                current_node.next = current_node.next.next
+            else:
+                current_node = current_node.next
+        return dum.next
