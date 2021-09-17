075acb723cd77f3d156f0bf6fcdebb9d81d8f9e7
NainaniJatinZ <nainani.jatin.0@gmail.com>
2021-09-17 23:03:33 +0530

    DS

--- leetcode/september/16-9/ImplementQueueusingStack.py
+++ leetcode/september/16-9/ImplementQueueusingStack.py
@@ -1,38 +1,47 @@
-class MyQueue(object):
+class MyQueue:
     
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        
-
+        self.stack1 = []
+        self.stack2 = []
     def push(self, x):
         """
         Push element x to the back of queue.
-        :type x: int
-        :rtype: None
         """
+        self.stack1.append(x)
         
 
     def pop(self):
         """
         Removes the element from in front of queue and returns that element.
-        :rtype: int
         """
-        
+        n = len(self.stack1) - 1
+        for i in range(n):
+            self.stack2.append(self.stack1.pop())
+        res = self.stack1.pop()
+        for i in range(n):
+            self.stack1.append(self.stack2.pop())
+        return res
 
     def peek(self):
         """
         Get the front element.
-        :rtype: int
         """
-        
+        n = len(self.stack1) - 1
+        for i in range(n):
+            self.stack2.append(self.stack1.pop())
+        res = self.stack1[0]
+        for i in range(n):
+            self.stack1.append(self.stack2.pop())
+        return res
 
     def empty(self):
         """
         Returns whether the queue is empty.
-        :rtype: bool
         """
+        return len(self.stack1) == 0
         
 
 
