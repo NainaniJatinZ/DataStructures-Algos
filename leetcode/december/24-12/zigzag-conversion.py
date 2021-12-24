cf1138c625a5bba583f881a0045a3068334937d5
NainaniJatinZ <nainani.jatin.0@gmail.com>
2021-12-24 18:41:54 +0530

    daily practise

---
+++ leetcode/december/24-12/zigzag-conversion.py
@@ -0,0 +1,25 @@
+# 6. Zigzag Conversion
+# Medium
+
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# And then read line by line: "PAHNAPLSIIGYIR"
+
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        
+        
+        
+if  __name__ == "__main__":
+    s = "PAYPALISHIRING"
+    numRows = 3
+    
+    
